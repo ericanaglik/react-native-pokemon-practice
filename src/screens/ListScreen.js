acf0821dbe306316274438f986a0372d92ea71b1
erica2605@live.com <erica2605@live.com>
2020-05-18 13:33:17 -0700

    pokemon search working

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,42 +1,102 @@
-import React from 'react'
-import { View, Text, StyleSheet, FlatList, Image, Button, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, ActivityIndicator } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import pokemon from './pokemon.js'
-import Bulba from './images/002.png'
-import { defaultProps } from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView';
 
-const ListScreen = () => {
-  
-  
-  // const img_url = './images/'
-  // const img = '003.png'
+class ListScreen extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View>
-      <Button
-      onPress={() => defaultProps.navigation.navigate('Component')}
-      title="Order Alphabet"/>
-    <FlatList
-      keyExtractor={pokemon => pokemon.name}
-      data={pokemon}
-      renderItem={({ item }) => {
-        return <Text style={styles.textStyle}><Image source={item.img_name} 
-        style={{width: 100, height: 100}}/>{item.name}</Text>
-        
-      }}
-      
-    />
-    
-    </View>
+    this.state = {
+      data: [],
+    };
 
-  )
-}
-
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 40,
-    marginVertical: 20
+    this.arrayholder = [];
   }
-})
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+
+
+        this.setState({
+          data: pokemon,
+        });
+
+        this.arrayholder = pokemon;
+
+
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
+
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
+
+    const newData = this.arrayholder.filter(pokemon => {
+      const itemData = `${pokemon.name.toUpperCase()}`;
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => {
+            return <Text><Image source={item.img_name} 
+            style={{width: 100, height: 100}}/>{item.name}</Text>
+            
+          }}
+          keyExtractor={pokemon => pokemon.name}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </View>
+    );
+  }
+}
 
 export default ListScreen;
 
